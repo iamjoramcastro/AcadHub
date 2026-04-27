da0d5a06abe90ecefe24b4345996738391cb96cf
iamjoramcastro <noiwillnotgiveittoyou@gmail.com>
2026-04-27 12:02:27 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='cnhs-hub-v7';
+const CACHE_VERSION='cnhs-hub-v8';
 const CORE_CACHE=CACHE_VERSION+'-core';
 const RUNTIME_CACHE=CACHE_VERSION+'-runtime';
 const FIREBASE_CACHE=CACHE_VERSION+'-firebase';
